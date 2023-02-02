a0995f2b21cbc39191b7888c3e184d62f3d9c5c2
Pragya <pragyapandey860@gmail.com>
2023-02-02 16:29:53 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
 import "../styles/App.css";
-
 const App = () => {
-  const [state, setState] = useState(true)
-  const handleToggle = () => {
-       if(state === true) {
-            setState(false)
-       } else {
-        setState(true)
-       }
-  }
+  //code here
+  const [buttonState, setButtonState] = useState({
+    id1: "OFF",
+    id2: "ON",
+  });
+
+  const handleClick = (e) => {
+    // console.log(e.target.value)
+    // console.log(typeof e.target.innerText);
+    if (e.target.id === "btn1") {
+      if (e.target.innerText === "OFF") {
+        setButtonState({
+          id1: "ON",
+          id2: "OFF",
+        });
+      } else {
+        setButtonState({
+          id1: "OFF",
+          id2: "ON",
+        });
+      }
+    } else {
+      if (e.target.innerText === "ON") {
+        setButtonState({
+          id1: "ON",
+          id2: "OFF",
+        });
+      } else {
+        setButtonState({
+          id1: "OFF",
+          id2: "ON",
+        });
+      }
+    }
+  };
+
   return (
-    <div id="main">
-      {state === true ? (
-        <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (
-        <div id="about-div">{<h1>Newton School is a neo-university providing highly
-           immersive tech learning to millions of students, to tap into new-age 
-           tech opportunities.</h1>}</div>
-        )}
-      <button id="toggle" onClick={handleToggle}>Toggle</button>
+    <div className="App">
+      <button id="btn1" onClick={handleClick}>
+        {/* assign value for button 1 */ buttonState.id1}
+      </button>
+      <br />
+      <br />
+      <button id="btn2" onClick={handleClick}>
+        {/* assign value for button 2 */ buttonState.id2}
+      </button>
     </div>
   );
 };
